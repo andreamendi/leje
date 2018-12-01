1cd4caea71349f8288b28cea15631ce683fabd29
AndreaMendi <andreamendi@icloud.com>
2018-12-01 12:18:16 -0600

    User MLH

--- leje/products/admin.py
+++ leje/products/admin.py
@@ -4,12 +4,14 @@ from .models import UserProfile, Direction
 
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user','cellphone') 
+    list_display = ('user','cellphone','bday') 
+
+
+# class UserDirectionAdmin(admin.ModelAdmin):
+#     list_display = ('street','int_number','ext_number','neighborhood','country','state')
+
 
 # Register your models here.
 admin.site.register(UserProfile, UserProfileAdmin)
 
 
-# class UserDirectionAdmin(admin.ModelAdmin):
-#     list_display = ('street','int_number','ext_number','neighborhood','country','state')
-# admin.site.register(UserProfile, UserDirectionAdmin)
